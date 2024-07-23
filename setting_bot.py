a2d1c0ae6aa133e9a10010bdd581087da1cb4215
Ruslab <164140261+RuslanKirillov@users.noreply.github.com>
2024-07-23 12:34:10 +1000

    Update setting_bot.py

--- setting_bot.py
+++ setting_bot.py
@@ -1,6 +1,6 @@
 api_TOKEN1 = '' #token
-msql_HOST1 = 'localhost' #host
-msql_USER1="root" #user
+msql_HOST1 = '' #host
+msql_USER1="" #user
 msql_PWD1="" #password
 msql_DATABASE="" #name base
-LOG_DIRECTORY="/Users/ruslankirillov/Documents/GitHub/std/logi" #log libary
+LOG_DIRECTORY="" #log libary

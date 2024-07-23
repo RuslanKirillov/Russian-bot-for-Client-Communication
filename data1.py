9061a2820f030c01a3cd275ec1171db2ec44a9b0
Ruslab <164140261+RuslanKirillov@users.noreply.github.com>
2024-07-23 20:32:36 +1000

    Create data1.py

---
+++ data1.py
@@ -0,0 +1,4 @@
+HELLO_TEXT = "change HELLO_TEXT in data1 file"
+BOT_INFO_TEXT = "change BOT_INFO_TEXT in data1 file"
+DONATE_BOT_TEXT = "change DONATE_BOT_TEXT in data1 file"
+SEND_BACKMESSAGE_TEXT = "change SEND_BACKMESSAGE_TEXT in data1 file"
